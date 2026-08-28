88711ffd128c5d837722cad111f5ff1891fd43d0
Sietze Soet <sietzesoet@gmail.com>
2026-08-28 14:54:02 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/france-war-trails/sw.js', { scope: '/france-war-trails/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/rondreis/sw.js', { scope: '/rondreis/' })})}
